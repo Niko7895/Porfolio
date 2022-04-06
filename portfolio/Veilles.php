7b75cd63ad64bd5aff99a08faebc29dba901e2ee
Nicolas CHALUMEAU <n.chalumeau@ecole-ipssi.net>
2022-04-07 00:51:08 +0200

    mise en place de la page veille et isertion de 2 veilles

--- portfolio/Veilles.php
+++ portfolio/Veilles.php
@@ -3,12 +3,71 @@
   <head>
     <title>Portfolio</title>
     <meta charset="utf8">
-    <link rel="stylesheet" href="CSS/Reseaux.css">
+    <link rel="stylesheet" href="CSS/veilles.css">
     <script></script>
   </head>
   <body>
   <?php
     include("header.php");
     ?>
+    <section>
+      <div class="veillesDiv">
+          <h1>Veille n°1</h1>
+          <div>
+            <h2>L'intelligence artificielle</h2>
+            <div>
+              <img class="preview" src="CSS/images/imgIA.png" alt="">
+            </div>
+            <div>
+              <h3>Description</h3>
+              <p class="description"></p>
+              <p>___________________</p>
+              <a href="CSS/veilles/veille-IA.pdf" download><h3>Télécharger la veille</h3></a>
+              <p>___________________</p>
+              <a href="CSS/veilles/presentation-veille-IA.pdf" target="blank"><h3>Voir la présentation</h3></a>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div>
+        <div class="veillesDiv">
+          <h1>Veille n°2</h1>
+          <div>
+            <h2>Technologies permettant de tester les performances d’un site</h2>
+            <div>
+              <img class="preview" src="CSS/images/imgTPS.png" alt="">
+            </div>
+            <div>
+              <h3>Description</h3>
+              <p class="description"></p>
+              <p>___________________</p>
+              <a href="CSS/veilles/veille-performances-site.pdf" download><h3>Télécharger la veille</h3></a>
+              <p>___________________</p>
+              <a href="CSS/veilles/presentation-veille-performances-site.pdf" target="blank"><h3>Voir la présentation</h3></a>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div>
+        <div class="veillesDiv">
+          <h1>Veille n°3</h1>
+          <div>
+            <h2>le langage Python</h2>
+            <div>
+              <img class="preview" src="CSS/images/imgIA.png" alt="">
+            </div>
+            <div>
+              <h3>Description</h3>
+              <p class="description"></p>
+              <p>___________________</p>
+              <a href="CSS/veilles/veille-IA.pdf" download><h3>Télécharger la veille</h3></a>
+              <p>___________________</p>
+              <a href="CSS/veilles/presentation-veille-IA.pdf" target="blank"><h3>Voir la présentation</h3></a>
+            </div>
+            <div>
+            </div>
+          </div>
+        </div>
+    </section>
   </body>
 </html>
